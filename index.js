d76947fd354d2f25ad72d7d7cd07286ab8ce4531
Arnav <arnavprajapati3101@gmail.com>
2026-04-19 19:16:24 +0530

    setup aws ec2 docker app

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from EC2 🚀');
+});
+
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on port 3000');
+});
